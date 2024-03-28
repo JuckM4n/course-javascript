337c9aea947cba7da64a19ea73083119d5fbe807
JuckM4n <nik_morozov0@mail.ru>
2024-03-28 22:24:12 +0300

    Week 2 is done

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,12 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el)); // выведет каждый элемент массива
  */
-function forEach() {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+  return array;
+}
 
 /*
  Задание 2:
@@ -21,7 +26,13 @@ function forEach() {}
    const newArray = map([1, 2, 3], (el) => el ** 2);
    console.log(newArray); // выведет [1, 4, 9]
  */
-function map() {}
+function map(el, fn) {
+  const newEl = [];
+  for (let i = 0; i < el.length; i++) {
+    newEl.push(fn(el[i], i, el));
+  }
+  return newEl;
+}
 
 /*
  Задание 3:
@@ -33,7 +44,23 @@ function map() {}
    const sum = reduce([1, 2, 3], (all, current) => all + current);
    console.log(sum); // выведет 6
  */
-function reduce() {}
+function reduce(elements, func, initial) {
+  let accumulator;
+  let startIndex;
+  if (initial !== undefined) {
+    accumulator = initial;
+    startIndex = 0;
+  } else {
+    accumulator = elements[0];
+    startIndex = 1;
+  }
+
+  for (let i = startIndex; i < elements.length; i++) {
+    accumulator = func(accumulator, elements[i], i, elements);
+  }
+
+  return accumulator;
+}
 
 /*
  Задание 4:
@@ -44,6 +71,12 @@ function reduce() {}
    const keys = upperProps({ name: 'Сергей', lastName: 'Петров' });
    console.log(keys) // выведет ['NAME', 'LASTNAME']
  */
-function upperProps() {}
+function upperProps(el) {
+  const newEl = [];
+  for (const key in el) {
+    newEl.push(key.toUpperCase());
+  }
+  return newEl;
+}
 
 export { forEach, map, reduce, upperProps };
