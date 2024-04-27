f621cc17c7308911e04602e2bcf96bec1cfa3178
JuckM4n <nik_morozov0@mail.ru>
2024-04-27 21:23:40 +0300

    Week 6 project is done

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -1,10 +1,10 @@
 const PERM_FRIENDS = 2;
 const PERM_PHOTOS = 4;
 //Мой VKApp_ID
-// const APP_ID = 51901591;
+const APP_ID = 51901591;
 
 //Преподовательский VKApp_ID
-const APP_ID = 5350105;
+// const APP_ID = 5350105;
 
 export default {
   getRandomElement(array) {
@@ -65,7 +65,6 @@ export default {
     });
   },
 
-  photoCache: {},
   callApi(method, params) {
     params.v = params.v || '5.199';
     return new Promise((resolve, reject) => {
@@ -91,6 +90,8 @@ export default {
     };
     return this.callApi('photos.getAll', params);
   },
+
+  photoCache: {},
   async getFriendPhotos(id) {
     let photos = this.photoCache[id];
 
@@ -118,5 +119,48 @@ export default {
     }
 
     return this.callApi('users.get', params);
+  },
+
+  async callServer(method, queryParams, body) {
+    queryParams = {
+      ...queryParams,
+      method,
+    };
+    const query = Object.entries(queryParams)
+      .reduce((all, [name, value]) => {
+        all.push(`${name}=${encodeURIComponent(value)}`);
+        return all;
+      }, [])
+      .join('&');
+    const params = {
+      headers: {
+        vk_token: this.token,
+      },
+    };
+
+    if (body) {
+      params.method = 'POST';
+      params.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(`/loft-photo/api/?${query}`, params);
+
+    return response.json();
+  },
+
+  async like(photo) {
+    return this.callServer('like', { photo });
+  },
+
+  async photoStats(photo) {
+    return this.callServer('photoStats', { photo });
+  },
+
+  async getComments(photo) {
+    return this.callServer('getComments', { photo });
+  },
+
+  async postComment(photo, text) {
+    return this.callServer('postComment', { photo }, { text });
   },
 };
